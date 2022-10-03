5b99874a0ae45244e34d3481a88a4f85149135c0
Leigh Johnson <leigh@bitsy.ai>
2022-10-02 17:48:10 -0700

    delete unused elements

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,9 @@
 use gst::glib;
-mod arrow_decoder;
-mod dataframe_decoder;
 mod dataframe_filesink;
 pub mod nnstreamer;
 pub mod tensor;
 
-#[allow(non_snake_case)]
-#[path = "../target/flatbuffers/nnstreamer_generated.rs"]
-pub mod message_nnstreamer;
-
 fn plugin_init(plugin: &gst::Plugin) -> Result<(), glib::BoolError> {
-    arrow_decoder::register(&plugin)?;
-    dataframe_decoder::register(plugin)?;
     dataframe_filesink::register(plugin)?;
     nnstreamer::register_nnstreamer_callbacks();
     Ok(())
